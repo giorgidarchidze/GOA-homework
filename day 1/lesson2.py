7ffe650153cef946b69eb8894711cc27eadd4da8
giorgidarchidze <darchidzegiorgi0@gmail.com>
2024-11-21 20:27:06 +0400

    first commit

---
+++ day 1/lesson2.py
@@ -0,0 +1,9 @@
+name = "giorgi"
+surname = "darchidze"
+
+age = 15
+heigt = 172.5
+
+city = "tbilis"
+
+print("chemi saxelia " + name + " " + surname + ", me var " + str(age) + " wlis, da vcxovrob " + city + " -shi")
